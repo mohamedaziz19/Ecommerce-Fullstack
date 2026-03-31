5efc9cad7df08d0ca1a67ca1d43f60d59c58aac3
Mohamed Aziz <118403675+mohamedaziz19@users.noreply.github.com>
2026-03-31 03:34:49 +0200

    Update MongoDB connection URI handling

--- Server (Node.JS - Express )/config/db.config.js
+++ Server (Node.JS - Express )/config/db.config.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+
+    const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/E-commerce";
+
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log("Database connected ✅");
   } catch (err) {
     console.error("Database connection failed:", err.message);

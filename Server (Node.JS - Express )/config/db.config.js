55f7871c7a3791ba84af8590a02252197e9065b2
Mohamed Aziz <118403675+mohamedaziz19@users.noreply.github.com>
2026-03-31 03:43:47 +0200

    Fix formatting in db.config.js

--- Server (Node.JS - Express )/config/db.config.js
+++ Server (Node.JS - Express )/config/db.config.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-
     const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/E-commerce";
-
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
     console.log("Database connected ✅");
   } catch (err) {
     console.error("Database connection failed:", err.message);
